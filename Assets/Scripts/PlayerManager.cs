1e818562715558229abe067279fa407080af8acb
希儿单推人 <hh75624777163@163.com>
2022-12-14 21:31:48 +0800

    搭了点地基

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public static PlayerManager instance;
+    public GameObject PlayerBody, PlayerHead, PlayerAll;
+    private bool isW, isD, isA, isS;
+    private float NowSpeed=0;
+    public float MaxSpeed,DeltaSpeed,d_DeltaSpeed;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            if (instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+    void Start()
+    {
+        PlayerBody = GameObject.Find("PlayerBody");
+        PlayerHead = GameObject.Find("PlayerHead");
+        PlayerAll = GameObject.Find("PlayerAll");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void Move()
+    {
+        if(NowSpeed<=MaxSpeed)
+        {
+            NowSpeed += DeltaSpeed;
+        }
+    }
+    private void ShootUp()
+    {
+
+    }
+}
