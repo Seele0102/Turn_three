ac149627b69951bd849fa45368163789818ac44f
流明的直肠好温暖 <113687741+Magosogold@users.noreply.github.com>
2023-01-04 16:18:48 +0800

    血条
    复刻了下像素图

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -1,27 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    public Image Bar;
-    public float health, maxHealth = 100;
-    private float lerpSpeed = 3;
+    public Image hpImage;
+    public Image hpEffectImage;
+
+    [HideInInspector]
+    public float hp;
+    //脚本挂在player下的canvas，当玩家受击时，other.GetComponentInChildren<HealthBar>().hp-=伤害
+    
+    [SerializeField] private float maxHp;
+    [SerializeField] private float hurtSpeed = 0.005f;
+
+    private void Start()
+    {
+        hp = maxHp;
+    }
+
     private void Update()
     {
-        BarFiller();
-    }
-    private void BarFiller()
-    {
-        Bar.fillAmount = Mathf.Lerp(Bar.fillAmount, health/maxHealth, lerpSpeed*Time.deltaTime);
-    }
-    private void AddHealth()
-    {
-
-    }
-    private void ReduceHealth()
-    {
+        hpImage.fillAmount = hp / maxHp;
+        if (hpEffectImage.fillAmount > hpImage.fillAmount)
+        {
+            hpEffectImage.fillAmount -= hurtSpeed;
+        }
+        else
+        {
+            hpEffectImage.fillAmount = hpImage.fillAmount;
+        }
 
     }
 }
