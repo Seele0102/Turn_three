3c72742aed69fc2b52e522b9ba4a78b8cf8cd948
流明的直肠好温暖 <113687741+Magosogold@users.noreply.github.com>
2023-01-09 20:22:49 +0800

    2.5d
    player脚本记得改改

---
+++ Assets/Scripts/RotatingCamera.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingCamera : MonoBehaviour
+{
+    public float rotateTime = 0.2f;
+    private Transform player;
+    private bool isRotating = false;
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;//记得在player那边加个tag
+    }
+
+    private void Update()
+    {
+        transform.position = player.position;
+        Rotate();
+    }
+
+    void Rotate()
+    {
+        if (Input.GetKeyDown(KeyCode.Q) && !isRotating)
+        {
+            StartCoroutine(RotateAround(-45, rotateTime));
+        }
+        if (Input.GetKeyDown(KeyCode.E) && !isRotating)
+        {
+            StartCoroutine(RotateAround(45, rotateTime));
+        }
+    }
+
+    IEnumerator RotateAround(float angel,float time)
+    {
+        float number = 60 * time;
+        float nextAngel = angel / number;
+        isRotating = true;
+
+        for(int i = 0; i < number; i++)
+        {
+            transform.Rotate(new Vector3(0, 0, nextAngel));
+            yield return new WaitForFixedUpdate();
+        }
+
+        isRotating=false;
+    }
+
+
+    /*player脚本
+     * inputX = Input.GetAxisRaw("Horizontal");
+     * inputY = Input.GetAxisRaw("Vertival");
+     * Vector2 input = (transform.right * inputX + transform.up * inputY).nomalized;
+     */
+
+
+}
