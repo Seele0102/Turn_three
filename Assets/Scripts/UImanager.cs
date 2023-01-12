bb8ea40785f4c57411eecba14c02cf7d74a13a39
LepusRattan <117378808+LepusRattan@users.noreply.github.com>
2023-01-12 13:30:47 +0800

    暂停先传一下

--- Assets/Scripts/UImanager.cs
+++ Assets/Scripts/UImanager.cs
@@ -1,16 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UImanager : MonoBehaviour
 {
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;  
     // Update is called once per frame
     void Update()
     {
@@ -22,5 +19,28 @@ public class UImanager : MonoBehaviour
         {
 
         }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+        GameIsPaused = true;
     }
 }
