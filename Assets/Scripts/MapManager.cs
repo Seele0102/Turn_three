f2d0bc5a55080ff0b88dfcc7821ac678a8cc64ad
希儿单推人 <hh75624777163@163.com>
2023-01-24 13:04:40 +0800

    0

---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    //记得写备注——阳
+    //本代码实现的需求功能
+    //1.实现地图的生成，即区块与区块的组合
+    //2.
+    //写备注的上课去了，晚上再来——1.24
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
