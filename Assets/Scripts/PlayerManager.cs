1e704a12dc7dc97bbbf997aea5dfaedd42eac555
希儿单推人 <hh75624777163@163.com>
2023-02-07 17:29:12 +0800

    0

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
@@ -7,23 +8,23 @@ using UnityEngine.UI;
 public class PlayerInfo/*角色信息*/
 {
     public int playerNumber;//判断当前角色
-    public bool isHead,isTired,isTiring;//判断现阶段状态
+    public bool isHead, isTired, isTiring;//判断现阶段状态
     public int level;//等级
     public float experence;//经验值
-    public float health,healthMax,healthDropRelief;//生命，最大生命，伤害减免
-    public float san, sanMax,sanMaxDrop,sanRise,sanMaxDropTime;//san，最大san，最大san下降
+    public float health, healthMax, healthDropRelief;//生命，最大生命，伤害减免
+    public float san, sanMax, sanMaxDrop, sanRise, sanMaxDropTime;//san，最大san，最大san下降
     public float relief;//闪避
     public float defense;//防御
-    public float attack,attackStrengthDrop,attackHit;//攻击力，攻击所需体力,攻击击退力度
+    public float attack, attackStrengthDrop, attackHit;//攻击力，攻击所需体力,攻击击退力度
     public float speed;//移动速度
-    public float sprintLength,sprintStrengthDrop,sprint, sprintMax;//冲刺距离，冲刺所需体力
-    public float strength,strengthMax,strengthDrop,strengthRise;//体力，最大体力，跑动所需体力，静止走动回复
+    public float sprintLength, sprintStrengthDrop, sprint, sprintMax;//冲刺距离，冲刺所需体力
+    public float strength, strengthMax, strengthDrop, strengthRise;//体力，最大体力，跑动所需体力，静止走动回复
     public float shoot, shootMax;
 }
 
 
 public class PlayerManager : MonoBehaviour
-{ 
+{
     public static PlayerManager instance;
     public static bool IsHead;//是否为头部
     public static PlayerInfo playerinfo;
@@ -33,16 +34,14 @@ public class PlayerManager : MonoBehaviour
     private Rigidbody2D HeadRB, BodyRB;
     public float sanDropen;
     public float ShootTime = 5, ShootMax = 5;
-    public float SprintTime=1,SprintMax=1;//头部弹射CD，冲刺CD
+    public float SprintTime = 1, SprintMax = 1;//头部弹射CD，冲刺CD
     public float H, V;
     private Vector3 move;
     private Vector3 Derection;//指向方向
     private Vector3 Angle;
     private float angle;
-    public static int EnemyNumber,BodyNum;
+    public static int EnemyNumber, BodyNum;
     private Quaternion Turn;//Player偏转
-    private bool FailToRun;
-
     //单例
     private void Awake()
     {
@@ -67,11 +66,10 @@ public class PlayerManager : MonoBehaviour
         HeadRB = PlayerHead.GetComponent<Rigidbody2D>();
         Player = GameObject.FindGameObjectWithTag("Player");
         PlayerHead.transform.parent = Player.transform;
-        PlayerHead.transform.localPosition= Vector3.zero; 
+        PlayerHead.transform.localPosition = Vector3.zero;
         PlayerHead.transform.localRotation = Quaternion.identity;
         EnemyNumber = 0;
         BodyNum = 0;
-        FailToRun= false;
     }
     void Update()
     {
@@ -82,11 +80,11 @@ public class PlayerManager : MonoBehaviour
         {
             HeadShot();
         }
-        if(Input.GetKeyDown(KeyCode.J))
+        if (Input.GetKeyDown(KeyCode.J))
         {
             Attack();
         }
-        if(Input.GetKeyDown(KeyCode.I)) 
+        if (Input.GetKeyDown(KeyCode.I))
         {
             OriginalSkill();
         }
@@ -96,7 +94,7 @@ public class PlayerManager : MonoBehaviour
     {
         if (PlayerBody == null)
         {
-            
+
             if (PlayerBody != null)
             {
                 IsHead = false;
@@ -116,38 +114,38 @@ public class PlayerManager : MonoBehaviour
         H = Input.GetAxisRaw("Horizontal");
         V = Input.GetAxisRaw("Vertical");
         Vector2 input = (transform.right * H + transform.up * V).normalized;
-        if (Input.GetKey(KeyCode.L)&&!FailToRun)
+        if (Input.GetKey(KeyCode.L))
         {
             playerinfo.speed = 0.004f;
             playerinfo.sprintLength = 5;
         }
         else
         {
-            playerinfo.speed= 0.002f;
+            playerinfo.speed = 0.002f;
             playerinfo.sprintLength = 2.5f;
         }
         if (H != 0 || V != 0)
         {
-            move.x = H*playerinfo.speed;
+            move.x = H * playerinfo.speed;
             move.y = V * playerinfo.speed;
             Player.transform.position += move;
             Angle.x = H;
             Angle.y = V;
-            if(V>=0)
+            if (V >= 0)
             {
-                angle= Vector3.Angle(new Vector3(1,0,0),Angle);
+                angle = Vector3.Angle(new Vector3(1, 0, 0), Angle);
             }
             else
             {
-                angle = 360f-Vector3.Angle(new Vector3(1, 0, 0), Angle);
+                angle = 360f - Vector3.Angle(new Vector3(1, 0, 0), Angle);
             }
             angle = 2 * angle * math.PI / 360f;
-            if (H>0)
+            if (H > 0)
             {
                 Turn.SetFromToRotation(new Vector3(1, 0, 0), new Vector3(-1, 0, 0));
                 Player.transform.rotation = Turn;
             }
-            else if (H<0)
+            else if (H < 0)
             {
                 Turn.SetFromToRotation(new Vector3(1, 0, 0), new Vector3(1, 0, 0));
                 Player.transform.rotation = Turn;
@@ -158,7 +156,7 @@ public class PlayerManager : MonoBehaviour
     //Done
     private void Sprint()
     {
-        if (Input.GetKeyDown(KeyCode.K)&&SprintTime>SprintMax)
+        if (Input.GetKeyDown(KeyCode.K) && SprintTime > SprintMax)
         {
             Derection.x = math.cos(angle) * playerinfo.sprintLength;
             Derection.y = math.sin(angle) * playerinfo.sprintLength;
@@ -168,7 +166,7 @@ public class PlayerManager : MonoBehaviour
     }
     private void OriginalSkill()
     {
-        if(playerinfo.playerNumber==1)
+        if (playerinfo.playerNumber == 1)
         {
 
         }
@@ -179,7 +177,7 @@ public class PlayerManager : MonoBehaviour
     }
     private void Attack()
     {
-        if(playerinfo.playerNumber == 1)
+        if (playerinfo.playerNumber == 1)
         {
 
         }
@@ -192,47 +190,67 @@ public class PlayerManager : MonoBehaviour
      10表示为伤害量*/
     public void Damage(float damage)
     {
+       //random
+       if(true)
+        {
+            damage = 0;
+        }
+       if(damage>playerinfo.defense&&damage-playerinfo.defense>=damage/20)
+        {
+            damage -= playerinfo.defense;
+        }
+       else
+        {
+            damage /=20f;
+        }
+        damage =damage* (100f-playerinfo.relief) / 100f;
         playerinfo.health -= damage;
     }
     //准备阶段各项属性的变化
     private void Prepare()
     {
-        if(playerinfo.isHead)
+        if (playerinfo.isHead)
         {
-            playerinfo.healthMax=playerinfo.health = 1;
-            if(sanDropen<playerinfo.sanMaxDrop&&playerinfo.san>=0)
+            playerinfo.healthMax = playerinfo.health = 1;
+            if (sanDropen < playerinfo.sanMaxDrop && playerinfo.san >= 0)
             {
-                sanDropen += (playerinfo.sanMax / playerinfo.sanMaxDropTime)*Time.deltaTime;
+                sanDropen += (playerinfo.sanMax / playerinfo.sanMaxDropTime) * Time.deltaTime;
             }
-            playerinfo.san -= sanDropen*Time.deltaTime;
+            playerinfo.san -= sanDropen * Time.deltaTime;
+            playerinfo.speed = 5;
         }
         else
         {
             sanDropen = 0;
-            if(playerinfo.san<playerinfo.sanMax)
+            if (playerinfo.san < playerinfo.sanMax)
             {
-                playerinfo.san += playerinfo.sanRise*Time.deltaTime;
+                playerinfo.san += playerinfo.sanRise * Time.deltaTime;
             }
+            playerinfo.speed = 10;
         }
-        if(playerinfo.shoot<playerinfo.shootMax)
+        if (playerinfo.shoot < playerinfo.shootMax)
         {
             ShootMax += Time.deltaTime;
         }
-
+        if (playerinfo.strength < playerinfo.strengthMax)
+        {
+            if (playerinfo.isTiring)
+            {
+                playerinfo.strength += 2 * playerinfo.strengthRise * Time.deltaTime;
+            }
+            else if (!playerinfo.isTired)
+            {
+                playerinfo.strength += playerinfo.strengthRise * Time.deltaTime;
+            }
+        }
     }
-    /*
-    public float attack, attackStrengthDrop, attackHit;//攻击力，攻击所需体力,攻击击退力度
-    public float speed;//移动速度
-    public float sprintLength, sprintStrengthDrop, sprint, sprintMax;//冲刺距离，冲刺所需体力
-    public float strength, strengthMax, strengthDrop, strengthRise;//体力，最大体力，跑动所需体力，静止走动回复
-    */
     //游戏初始面板调整
     private void GameStart(PlayerInfo p)
     {
-        p.isHead = false;
+        p.isHead = p.isTired = p.isTiring = false;
         p.level = 1;
-        p.experence= 0;
-        p.health =p.healthMax= 100;
+        p.experence = 0;
+        p.health = p.healthMax = 100;
         p.healthDropRelief = 0;
         p.san = p.sanMax = 100;
         p.sanMaxDrop = 10;
@@ -242,11 +260,11 @@ public class PlayerManager : MonoBehaviour
         p.defense = 10;
         p.attack = 10;
         p.attackStrengthDrop = 2;
-        if(p.playerNumber==1)
+        if (p.playerNumber == 1)
         {
             p.attackHit = 5;
         }
-        else if(p.playerNumber==2)
+        else if (p.playerNumber == 2)
         {
             p.attackHit = 1;
         }
